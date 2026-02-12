0ae9a28c6f9a3d4412087ec4a4c17da3ac438cf5
Arturo Villalobos <artuvillam@gmail.com>
2026-02-11 23:17:48 -0600

    Initial Commit

---
+++ resbar/resbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  resbarApp.swift
+//  resbar
+//
+//  Created by Arturo  Villalobos on 2/11/26.
+//
+
+import SwiftUI
+
+@main
+struct resbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ resbarTests/resbarTests.swift
@@ -0,0 +1,17 @@
+//
+//  resbarTests.swift
+//  resbarTests
+//
+//  Created by Arturo  Villalobos on 2/11/26.
+//
+
+import Testing
+@testable import resbar
+
+struct resbarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
